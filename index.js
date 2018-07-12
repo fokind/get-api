309c02a31b0b07a1df4c3751397a10385fe99c20
Dmitry Fokin <fokind1@yandex.ru>
2018-07-12 18:06:17 +0300

    рефакторинг

--- index.js
+++ index.js
@@ -1,113 +1,112 @@
 import { MongoClient } from 'mongodb';
 import RestRequest from 'rest-request';
-import { NeuralNetwork } from 'brain.js';
+// import { NeuralNetwork } from 'brain.js';
 
-import { Layer, Network } from 'synaptic';
+// import { Layer, Network } from 'synaptic';
 
-let inputLayer = new Layer(3);
-let hiddenLayer = new Layer(5);
-let outputLayer = new Layer(1);
+/* const inputLayer = new Layer(3);
+const hiddenLayer = new Layer(5);
+const outputLayer = new Layer(1); */
 
-inputLayer.project(hiddenLayer);
-hiddenLayer.project(outputLayer);
+/* inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer); */
 
-let myNetwork = new Network({
-	input: inputLayer,
-	hidden: [hiddenLayer],
-	output: outputLayer
-});
+/* const myNetwork = new Network({
+  input: inputLayer,
+  hidden: [hiddenLayer],
+  output: outputLayer,
+}); */
 
-let learningRate = 0.3;
+// const learningRate = 0.3;
 
-let config = {
-};
-//create a simple recurrent neural network
-//var net = new brain.recurrent.LSTM(config);
-let net = new NeuralNetwork(config);
+/* const config = {
+}; */
+// create a simple recurrent neural network
+// var net = new brain.recurrent.LSTM(config);
+// const net = new NeuralNetwork(config);
 
 
-const load = async () => {
+/* const load = async () => {
   const a = await Promise.resolve(5);
   const b = await Promise.resolve(10);
   return a + b;
-};
-
+}; */
 
 
 // сбор статистики
-let restApi = new RestRequest('https://min-api.cryptocompare.com/data');
-let histodayPromise = restApi.get('histoday', {
+const restApi = new RestRequest('https://min-api.cryptocompare.com/data');
+const histodayPromise = restApi.get('histoday', {
   fsym: 'BTC',
   tsym: 'USD',
-  limit: 2000
+  limit: 2000,
 });
 
 // var allCoinlistPromise = restApi.get('all/coinlist');
 
 // сохранение в базу данных
 const url = 'mongodb://readWrite:readWrite@localhost:27017/crypto';
-let clientPromise = MongoClient.connect(url, {useNewUrlParser: true});
+const clientPromise = MongoClient.connect(url, { useNewUrlParser: true });
 
-Promise.all([histodayPromise, clientPromise]).then(values => {
-  let histoday = values[0];
-  let client = values[1];
+Promise.all([histodayPromise, clientPromise]).then((values) => {
+  const histoday = values[0];
+  const client = values[1];
 
   console.log(new Date(histoday.Data[0].time * 1000));
   console.log(new Date(histoday.Data[2000].time * 1000));
 
-  let data = histoday.Data.map(e => {
+  const data = histoday.Data.map((e) => {
     e.date = new Date(e.time * 1000);
     e.fsym = 'BTC';
     e.tsym = 'USD';
     return e;
   });
 
-  let prev = 0;
-  let post = 1;
-  let to = data.length - post;
+  const prev = 0;
+  const post = 1;
+  const to = data.length - post;
 
-  for (let i = prev; i < to; i++) {
-    let next = data[i + 1];
-    let current = data[i];
+  for (let i = prev; i < to; i += 1) {
+    const next = data[i + 1];
+    const current = data[i];
 
-    //var sign = current.close >= current.open; // направление
-    let max = current.close > current.open ? current.close : current.open; // верхняя граница свечи
-    let min = current.close < current.open ? current.close : current.open; // нижняя граница свечи
-    let sign = (current.close - current.open) / current.high;
-    let total = current.high - current.low; // полный размер
-    let body = (max - min) / total; // доля тела
-    let top = (sign >= 0 ? (current.high - max) : (min - current.low)) / total; // доля верхнего фитиля
-    let bottom = (sign < 0 ? (current.high - max) : (min - current.low)) / total; // доля нижнего фитиля
+    // var sign = current.close >= current.open; // направление
+    const max = current.close > current.open ? current.close : current.open; // верхняя граница свечи
+    const min = current.close < current.open ? current.close : current.open; // нижняя граница свечи
+    const sign = (current.close - current.open) / current.high;
+    const total = current.high - current.low; // полный размер
+    const body = (max - min) / total; // доля тела
+    const top = (sign >= 0 ? (current.high - max) : (min - current.low)) / total; // доля верхнего фитиля
+    const bottom = (sign < 0 ? (current.high - max) : (min - current.low)) / total; // доля нижнего фитиля
 
     data[i].input = {
-      sign: sign,
-      top: top,
-      body: body,
-      bottom: bottom
+      sign,
+      top,
+      body,
+      bottom,
     };
 
-    let good = next.high > current.high ? 1 : 0;
-    let bad = next.low < current.low ? -1 : 0;
+    const good = next.high > current.high ? 1 : 0;
+    const bad = next.low < current.low ? -1 : 0;
 
     data[i].output = {
-      good: good,
-      bad: bad
+      good,
+      bad,
     };
 
-    /*if (i < 1900) net.train([{input: [sign, top, bottom], output: [good]}]);
+    /* if (i < 1900) net.train([{input: [sign, top, bottom], output: [good]}]);
     else {
       var output = net.run([sign, top, bottom]);
       console.log('test good: ', good, ' - ', output[0]);//, ', bad: ', bad, ' - ', output[1]);
-    }*/
+    } */
 
-    /*if (i < 1900) {
+    /* if (i < 1900) {
       myNetwork.activate([sign, top, bottom]);
-	    myNetwork.propagate(learningRate, [good]);
+      myNetwork.propagate(learningRate, [good]);
     }
     else {
       var output = myNetwork.activate([sign, top, bottom]);
       console.log('test good: ', good, ' - ', output[0]);
-    }*/
+    } */
 
 
     // next > current: 100% позитивный
@@ -128,12 +127,10 @@ Promise.all([histodayPromise, clientPromise]).then(values => {
     // good = next.high - current.high + current.low - next.low;
   }
 
-  //console.log(net.toJSON());
+  // console.log(net.toJSON());
 
-  let db = client.db('crypto');
-  let collection = db.collection('histoday');
+  const db = client.db('crypto');
+  const collection = db.collection('histoday');
 
-  collection.drop().then(() =>
-    collection.insertMany(data).then(() => client.close())
-  );
+  collection.drop().then(() => collection.insertMany(data).then(() => client.close()));
 });
