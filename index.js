e9da1637bd93da50a27ba10302933160b878671d
Dmitry Fokin <fokind1@yandex.ru>
2018-07-13 10:22:34 +0300

    работает асинхронное подключение к монго и зарпос из api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { MongoClient } from 'mongodb';
-import RestRequest from 'rest-request';
+const MongoClient = require('mongodb');
+// const RestRequest = require('rest-request');
+const async = require('async');
+const rp = require('request-promise');
+
 // import { NeuralNetwork } from 'brain.js';
 
 // import { Layer, Network } from 'synaptic';
@@ -25,34 +28,118 @@ hiddenLayer.project(outputLayer); */
 // var net = new brain.recurrent.LSTM(config);
 // const net = new NeuralNetwork(config);
 
-
 /* const load = async () => {
   const a = await Promise.resolve(5);
   const b = await Promise.resolve(10);
   return a + b;
 }; */
 
-
 // сбор статистики
-const restApi = new RestRequest('https://min-api.cryptocompare.com/data');
+/* const restApi = new RestRequest('https://min-api.cryptocompare.com/data');
 const histodayPromise = restApi.get('histoday', {
   fsym: 'BTC',
   tsym: 'USD',
   limit: 2000,
-});
+}); */
+
+const fsym = 'BTC';
+const tsym = 'USD';
+const limit = 1;
+
+const request = require('request');
+
+/* request(`https://min-api.cryptocompare.com/data/histoday?fsym=${fsym}&tsym=${tsym}&limit=${limit}`, (error, response, body) => {
+  // console.log('error:', error);
+  // console.log('statusCode:', response && response.statusCode);
+  let jBody = JSON.parse(body);
+  console.log('body:', jBody.Data);
+  //console.log('Data:', body.Data);
+  console.log('Data:', typeof jBody.Data);
+}); */
+
+/* const load = async () => {
+  const a = await Promise.resolve(5);
+  const b = await Promise.resolve(10);
+  return a + b;
+};
+
+load().then(value => console.log(value)); // 15 */
+
+
+const dbPromise = MongoClient.connect('mongodb://readWrite:readWrite@localhost:27017/crypto', { useNewUrlParser: true });
+
+let count = 0;
+let toTs;
+
+const loadAll = async.whilst(
+  () => {
+    console.log(count);
+    return count <= 1;
+  },
+  (callback) => {
+    count += 1;
+    const apiPromise = rp(`https://min-api.cryptocompare.com/data/histoday?fsym=${fsym}&tsym=${tsym}&limit=${limit}${toTs ? `&toTs=${toTs}` : ''}`);
+    Promise.all([dbPromise, apiPromise]).then((values) => {
+      console.log(values);
+      /* const db = values[0];
+      const response = values[1];
+      const jBody = JSON.parse(response);
+      toTs = jBody.TimeFrom;
+      console.log(toTs); */
+      callback(null, count);
+    });
+  },
+  () => {
+    // console.log(n);
+  },
+);
+
+/* Promise.all([dbPromise, apiPromise]).then((values) => {
+  let mongo = values[0];
+
+  console.log(values);
+}); */
+
+// будем последовательно
+// заполнить базу данных исходными данными, весь временной ряд по заданной валюте полностью
+// не дожидаемся подключения к монго
+// выполняем последовательно запросы к апи до тех пор, пока количество извлеченных данных не станет меньше лимита
+// после каждого полученного результата отправляем его в монго
+// для этого синхронизируем два промиса подключение к монго и получение результатов
+
+
+// console.log('1');
+
+/* let count = 0;
+whilst(
+  () => count < 5,
+  (callback) => {
+    count += 1;
+    setTimeout(() => {
+      callback(null, count);
+    }, 1000);
+  },
+  () => {},
+); */
 
 // var allCoinlistPromise = restApi.get('all/coinlist');
 
 // сохранение в базу данных
-const url = 'mongodb://readWrite:readWrite@localhost:27017/crypto';
-const clientPromise = MongoClient.connect(url, { useNewUrlParser: true });
+/* const url = "mongodb://readWrite:readWrite@localhost:27017/crypto";
+const clientPromise = MongoClient.connect(
+  url,
+  {
+    useNewUrlParser: true
+  }
+); */
 
-Promise.all([histodayPromise, clientPromise]).then((values) => {
+/* Promise.all([histodayPromise, clientPromise]).then((values) => {
   const histoday = values[0];
   const client = values[1];
 
-  console.log(new Date(histoday.Data[0].time * 1000));
-  console.log(new Date(histoday.Data[2000].time * 1000));
+  // console.log(new Date(histoday.Data[0].time * 1000));
+  // console.log(new Date(histoday.Data[2000].time * 1000));
+  console.log(histoday);
 
   const data = histoday.Data.map((e) => {
     e.date = new Date(e.time * 1000);
@@ -65,7 +152,7 @@ Promise.all([histodayPromise, clientPromise]).then((values) => {
   const post = 1;
   const to = data.length - post;
 
-  for (let i = prev; i < to; i += 1) {
+  for (let i = prev; i < to; i++) {
     const next = data[i + 1];
     const current = data[i];
 
@@ -99,38 +186,37 @@ Promise.all([histodayPromise, clientPromise]).then((values) => {
       console.log('test good: ', good, ' - ', output[0]);//, ', bad: ', bad, ' - ', output[1]);
     } */
 
-    /* if (i < 1900) {
-      myNetwork.activate([sign, top, bottom]);
-      myNetwork.propagate(learningRate, [good]);
-    }
-    else {
-      var output = myNetwork.activate([sign, top, bottom]);
-      console.log('test good: ', good, ' - ', output[0]);
-    } */
+/* if (i < 1900) {
+  myNetwork.activate([sign, top, bottom]);
+  myNetwork.propagate(learningRate, [good]);
+}
+else {
+  var output = myNetwork.activate([sign, top, bottom]);
+  console.log('test good: ', good, ' - ', output[0]);
+} */
 
+// next > current: 100% позитивный
+// next < current: 100% негативный
+// next поглощает current и наоборот: 100% неопределенность
+// остальные частично
 
-    // next > current: 100% позитивный
-    // next < current: 100% негативный
-    // next поглощает current и наоборот: 100% неопределенность
-    // остальные частично
+// риск негативного исхода
+// next.high < current.low = -1
+// next.low >= current.high = 0
+// total = current.high - next.low;
+// bad = current.high - next.high + current.low - next.low;
 
-    // риск негативного исхода
-    // next.high < current.low = -1
-    // next.low >= current.high = 0
-    // total = current.high - next.low;
-    // bad = current.high - next.high + current.low - next.low;
+// вероятность позитивного исхода
+// next.high < current.low = 0
+// next.low > current.high = 1
+// total = next.high - current.low;
+// good = next.high - current.high + current.low - next.low;
+// }
 
-    // вероятность позитивного исхода
-    // next.high < current.low = 0
-    // next.low > current.high = 1
-    // total = next.high - current.low;
-    // good = next.high - current.high + current.low - next.low;
-  }
+// console.log(net.toJSON());
 
-  // console.log(net.toJSON());
-
-  const db = client.db('crypto');
+/* const db = client.db('crypto');
   const collection = db.collection('histoday');
 
   collection.drop().then(() => collection.insertMany(data).then(() => client.close()));
-});
+}); */
